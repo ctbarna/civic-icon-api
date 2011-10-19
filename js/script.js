aea3159cd5005b59255099be521131da60714772
Chris Barna <chris@unbrain.net>
2011-10-19 14:37:47 -0700

    Change size of icons on live updater.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,17 @@ $(document).ready(function () {
   $(".icon").iconReplacer(50);
 
   // Live update stuff.
+  var liveUpdateSize = 20;
   $('#try-me-update').html($('#try-me').val());
   $("#try-me").keyup(function () {
     console.log($(this).val());
     $('#try-me-update').html($(this).val());
-    $('#try-me-update').iconReplacer(20);
+    $('#try-me-update').iconReplacer(liveUpdateSize);
+  });
+
+  $("#try-me-selector").change(function () {
+    liveUpdateSize = $(this).val();
+    $("#try-me-update").html($("#try-me").val());
+    $("#try-me-update").iconReplacer(liveUpdateSize);
   });
 });
